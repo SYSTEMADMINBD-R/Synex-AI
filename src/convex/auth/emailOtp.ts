ce6ddcbc6cbe580391e056565f36b927f45005db
SYSTEMADMINBD <traderrn2007@gmail.com>
2026-09-13 03:02:27 +0600

    Update emailOtp.ts

--- src/convex/auth/emailOtp.ts
+++ src/convex/auth/emailOtp.ts
@@ -22,7 +22,7 @@ export const emailOtp = Email({
         {
           to: email,
           otp: token,
-          appName: "TwinMind",
+          appName: "XIREVO",
         },
         {
           headers: {
